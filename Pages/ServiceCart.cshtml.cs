43f244a34d273b0ec4b0fdd9592562a079ddd986
aizpop <airahbaculo105@gmail.com>
2023-04-12 14:51:53 +0800

    hello airah 2nd commit

---
+++ Pages/ServiceCart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AvengerSquad_HardwareNI.Pages
+{
+    public class ServiceCartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/ServiceDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AvengerSquad_HardwareNI.Pages
+{
+    public class ServiceDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
